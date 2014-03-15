434c52f8305ba52b8de1edbde50cd5c34374f347
Hiroshi Sugawara <5000164@users.noreply.github.com>
2014-03-15 13:11:31 +0900

    SelectorをRenderingクラスに統合

--- rendering.cc
+++ rendering.cc
@@ -2,17 +2,12 @@
 #include <string>
 #include "dx_lib_wrapper.h"
 #include "font.h"
-#include "selector.h"
-
-Rendering::Rendering()
-{
-}
 
 void Rendering::BattleMenu(int command_selector, std::string menu1, std::string menu2)
 {
 	// ‰æ–Ê•`‰æ
 	Font font;
-	Selector selector;
+
 	SetDrawScreen(DX_SCREEN_BACK);
 	ClearDrawScreen();
 	font.Draw(menu1, 50, 50);
@@ -20,30 +15,35 @@ void Rendering::BattleMenu(int command_selector, std::string menu1, std::string 
 
 	if (command_selector == 1)
 	{
-		selector.Draw(35, 49, 35, 69, 45, 59, 230, 230, 230);
+		int color_dx = GetColor(230, 230, 230);
+		DrawTriangle(35, 49, 35, 69, 45, 59, color_dx, FALSE);
 	}
 	else if (command_selector == 2)
 	{
-		selector.Draw(35, 99, 35, 119, 45, 109, 230, 230, 230);
+		int color_dx = GetColor(230, 230, 230);
+		DrawTriangle(35, 99, 35, 119, 45, 109, color_dx, FALSE);
 	}
 
 	ScreenFlip();
+
+	return;
 }
 
 void Rendering::BattleMessage(int command_selector, int result, std::string result_text, std::string message_text1, std::string message_text2, std::string c1_hit_point, std::string c2_hit_point)
 {
 	Font font;
-	Selector selector;
 
 	// ‚½‚½‚©‚¤
 	if (command_selector == 1)
 	{
-		selector.Draw(35, 49, 35, 69, 45, 59, 230, 230, 230);
+		int color_dx = GetColor(230, 230, 230);
+		DrawTriangle(35, 49, 35, 69, 45, 59, color_dx, FALSE);
 	}
 	// ‚É‚°‚é
 	else if (command_selector == 2)
 	{
-		selector.Draw(35, 99, 35, 119, 45, 109, 230, 230, 230);
+		int color_dx = GetColor(230, 230, 230);
+		DrawTriangle(35, 99, 35, 119, 45, 109, color_dx, FALSE);
 	}
 
 	SetDrawScreen(DX_SCREEN_FRONT);
@@ -52,4 +52,6 @@ void Rendering::BattleMessage(int command_selector, int result, std::string resu
 	font.Draw(message_text2, 100, 210);
 	font.Draw(c1_hit_point, 250, 180);
 	font.Draw(c2_hit_point, 250, 210);
+
+	return;
 }
